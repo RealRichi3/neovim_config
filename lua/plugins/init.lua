9ce565512e3c7570ac1fe7b175eeb24c96bc2d6f
Richie <molunorichie@gmail.com>
2024-07-16 01:11:44 +0100

    set theme to gruvbox

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,35 @@ local default_plugins = {
     { 'wakatime/vim-wakatime', lazy = false },
     "nvim-lua/plenary.nvim",
     { "sonjiku/yawnc.nvim" },
+{
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function()
+            require("gruvbox").setup({
+    terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,            })
+            end,
+    },
     {
         "ThePrimeagen/harpoon",
         enabled = true,
