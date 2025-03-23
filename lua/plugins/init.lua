10eab692e89d3c60781705c7919ce77fcbc1117e
Richie <molunorichie@gmail.com>
2025-03-23 04:25:01 +0100

    add: alpha config for welcome banner

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,7 @@
 local default_plugins = {
     { 'wakatime/vim-wakatime', lazy = false },
     { "nvim-lua/plenary.nvim" },
+    { "folke/snacks.nvim",     opts = { dashboard = { enabled = false } } },
     {
         "dapt4/vim-autoSurround",
         lazy = false,
@@ -414,6 +415,30 @@ local default_plugins = {
                 })
             end, { desc = "format file or range" })
         end,
+    },
+
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
     },
 }
 
