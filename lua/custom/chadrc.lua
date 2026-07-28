67244671977b170f980d791ec0892f98cfe171ea
RealRichi3 <molunorichie@gmail.com>
2026-07-28 23:05:08 +0100

    hide cwd and cursor position in statusline

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,20 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'aquarium' }
+M.ui = {
+    theme = 'aquarium',
+
+    statusline = {
+        -- overriden_modules receives the module list by reference and mutates it.
+        -- Index map (see nvchad/statusline/default.lua):
+        --   1 mode        2 fileInfo   3 git          4 "%="
+        --   5 LSP_progress 6 "%="      7 LSP_Diagnostics
+        --   8 LSP_status  9 cwd       10 cursor_position
+        overriden_modules = function(modules)
+            modules[9] = ""  -- 󰉋 folder name
+            modules[10] = "" -- Top / Bot / NN%
+        end,
+    },
+}
 
 return M
